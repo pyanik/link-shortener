71c525c93b9ad9efb6e4ac5cfcc89cc3138370b0
pyanik <pyanik@users.noreply.github.com>
2022-04-03 16:50:59 +0200

    H2 Database Connection

--- src/main/java/dev/pyanik/linkshortener/link/LinkServiceImpl.java
+++ src/main/java/dev/pyanik/linkshortener/link/LinkServiceImpl.java
@@ -2,26 +2,32 @@ package dev.pyanik.linkshortener.link;
 
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class LinkServiceImpl implements LinkService {
 
-    private final Map<String, LinkDto> repository = new HashMap<>();
+    private final LinkRepository repository;
+
+    LinkServiceImpl(LinkRepository repository) {
+        this.repository = repository;
+    }
 
 
     @Override
     public LinkDto createLink(LinkDto dto) throws LinkAlreadyExistsException {
-        if (repository.containsKey(dto.id())) {
+        Optional<LinkEntity> linkEntity = repository.findById(dto.id());
+        if (linkEntity.isPresent()) {
             throw new LinkAlreadyExistsException(dto.id());
         }
-        repository.put(dto.id(), dto);
+        repository.save(LinkEntity.fromDto(dto));
         return dto;
     }
 
     @Override
     public LinkDto getLink(String id) {
-        return repository.get(id);
+        return repository.findById(id)
+                .map(LinkEntity::toDto)
+                .orElse(null);
     }
 }
